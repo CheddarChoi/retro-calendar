35f3857fdeaa5eccb02810cff2a8318ca4969f64
CheddarChoi <cde1103@kaist.ac.kr>
2020-11-16 01:08:04 +0900

    set page directory

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, Redirect, BrowserRouter } from "react-router-dom";
+import MainPage from "./components/pages/MainPage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/main" component={MainPage} />
+        <Redirect to="/main" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        fontFamily: "NanumSquare"
+      }}
+      className="d-flex flex-column"
+    >
+      hello
+    </div>
+  );
+};
+
+export default MainPage;
